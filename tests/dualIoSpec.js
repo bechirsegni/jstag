7d5d1cc43d03125a8a38a5c68905e18a4979ca0e
Mark Hayden <hi@markhayden.me>
2016-06-17 12:29:19 -0700

    updated dual cid test params

--- tests/dualIoSpec.js
+++ tests/dualIoSpec.js
@@ -2,7 +2,7 @@ describe("verify that we can handle an array of cid", function() {
   var async1, count=0;
 
   function testSend(done) {
-    jstag.send("test", {"one":"one"}, function(opts, self){
+    jstag.send("test", {"one":"value1"}, function(opts, self){
       async1 = opts;
       count++;
       if(count >= 2){
@@ -17,6 +17,6 @@ describe("verify that we can handle an array of cid", function() {
 
   it("should send the same data to two accounts", function () {
     expect(async1.sendurl).toEqual(['//c.lytics.io/c/bogusaccountid2/test', '//c.lytics.io/c/bogusaccountid/test']);
-    expect(async1.dataMsg).toEqual('one=one&_ts=' + async1.data._ts + '&_nmob=t&_device=desktop&url=localhost%3A9976%2Fcontext.html&_if=t&_v='+window.__karma__.config.ioversion+'&_e=pv&_sesstart=1&_tz=-7&_ul=en-US&_sz=1024x768&_uid=' + async1.data._uid + '&_getid=t&_ca=jstag1')
+    expect(async1.dataMsg).toEqual('one=value1&_ts=' + async1.data._ts + '&_nmob=t&_device=desktop&url=localhost%3A9976%2Fcontext.html&_if=t&_v='+window.__karma__.config.ioversion+'&_e=pv&_sesstart=1&_tz=-7&_ul=en-US&_sz=1024x768&_uid=' + async1.data._uid + '&_getid=t&_ca=jstag1')
   });
 });
